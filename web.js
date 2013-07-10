d5cac645043356978802e0c943f1f8953c9eaffd
Vikas Gupta <gvikas91@gmail.com>
2013-07-10 12:58:52 +0000

    Added fs.readFileSync and Buffer.toString

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
-var express = require('express');
+express var = require('express');
+var fs = require('fs');
+var buffer = Buffer('utf-8');
+
+var rf = fs.readFileSync("index.html");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(rf.toString());
 });
 
 var port = process.env.PORT || 5000;
